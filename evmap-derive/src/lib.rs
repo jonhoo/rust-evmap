cb8500a7a81b8fa199a3b7f9e6f052036f4e3091
Maya <maya@f-list.net>
2020-03-26 19:16:24 +0100

    add generic tests and fmt the derive crate too

--- evmap-derive/src/lib.rs
+++ evmap-derive/src/lib.rs
@@ -1,7 +1,9 @@
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index};
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index,
+};
 
 #[proc_macro_derive(ShallowCopy)]
 pub fn derive_shallow_copy(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -22,7 +24,9 @@ pub fn derive_shallow_copy(input: proc_macro::TokenStream) -> proc_macro::TokenS
 fn add_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
         if let GenericParam::Type(ref mut type_param) = *param {
-            type_param.bounds.push(parse_quote!(evmap::shallow_copy::ShallowCopy));
+            type_param
+                .bounds
+                .push(parse_quote!(evmap::shallow_copy::ShallowCopy));
         }
     }
     generics
@@ -30,35 +34,31 @@ fn add_trait_bounds(mut generics: Generics) -> Generics {
 
 fn fields(data: &Data, type_name: &Ident) -> TokenStream {
     match data {
-        Data::Struct(data) => {
-            match &data.fields {
-                Fields::Named(fields) => {
-                    let recurse = fields.named.iter().map(|f| {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => {
+                let recurse = fields.named.iter().map(|f| {
                         let name = &f.ident;
                         quote_spanned! {f.span()=>
                             #name: std::mem::ManuallyDrop::into_inner(evmap::shallow_copy::ShallowCopy::shallow_copy(&self.#name))
                         }
                     });
-                    quote! {
-                        std::mem::ManuallyDrop::new(Self { #(#recurse,)* })
-                    }
+                quote! {
+                    std::mem::ManuallyDrop::new(Self { #(#recurse,)* })
                 }
-                Fields::Unnamed(fields) => {
-                    let recurse = fields.unnamed.iter().enumerate().map(|(i, f)| {
+            }
+            Fields::Unnamed(fields) => {
+                let recurse = fields.unnamed.iter().enumerate().map(|(i, f)| {
                         let index = Index::from(i);
                         quote_spanned! {f.span()=>
                             std::mem::ManuallyDrop::into_inner(evmap::shallow_copy::ShallowCopy::shallow_copy(&self.#index))
                         }
                     });
-                    quote! {
-                        std::mem::ManuallyDrop::new(#type_name(#(#recurse,)*))
-                    }
-                }
-                Fields::Unit => {
-                    quote!(std::mem::ManuallyDrop::new(#type_name))
+                quote! {
+                    std::mem::ManuallyDrop::new(#type_name(#(#recurse,)*))
                 }
             }
-        }
+            Fields::Unit => quote!(std::mem::ManuallyDrop::new(#type_name)),
+        },
         Data::Enum(data) => {
             let recurse = data.variants.iter().map(|f| {
                 let (names, fields) = match &f.fields {
--- evmap-derive/tests/failing/lib.rs
+++ evmap-derive/tests/failing/lib.rs
@@ -1,4 +1,5 @@
 use evmap_derive::ShallowCopy;
+use evmap::shallow_copy::ShallowCopy as _;
 
 #[derive(ShallowCopy)]
 struct Broken {
@@ -9,4 +10,16 @@ struct Broken {
 #[derive(ShallowCopy)]
 struct AlsoBroken(i32, std::cell::Cell<()>);
 
-fn main() {}
+#[derive(ShallowCopy)]
+struct Generic<T> {
+	f1: T
+}
+
+fn main() {
+	let broken = Broken { f1: 0, f2: std::cell::Cell::new(()) };
+	broken.shallow_copy();
+	let also_broken = AlsoBroken(0, std::cell::Cell::new(()));
+	broken.shallow_copy();
+	let generic = Generic { f1: std::cell::Cell::new(()) };
+	generic.shallow_copy();
+}
