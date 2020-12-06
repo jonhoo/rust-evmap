bdab7caf87d5a9bb5f06cad5aa9bc8b232bae5fa
Jon Gjengset <jon@thesquareplanet.com>
2020-12-05 17:31:33 -0800

    Actually, pub NoDrop is fine as long as the module is private

--- evmap/src/aliasing.rs
+++ evmap/src/aliasing.rs
@@ -1,11 +1,20 @@
+//! The types in this module _cannot_ be public, and you must _never_ implement a trait for one but
+//! not the other.
+//!
+//! See the module-level documentation on [`left_right::aliasing`] for details.
+
 use left_right::aliasing::DropBehavior;
 
-// NOTE: These types _cannot_ be public, as doing so may cause external implementations to
-// implement different behavior for them, which would make transmutes between
-// SomeWrapper<Aliased<T, NoDrop>> and SomeWrapper<Aliased<T, DoDrop>> unsound.
-#[doc(hidden)]
+/// If you can see this type outside of `evmap`, that is a severe unsoundness bug.
+/// Please report it at https://github.com/jonhoo/rust-evmap/issues.
 #[allow(missing_debug_implementations)]
+// NOTE: This type is pub so that we can expose it in a couple of trait bounds of the form:
+//
+//   Aliased<T, NoDrop>: Trait
+//
+// However, it remains private because this module is private.
 pub struct NoDrop;
+
 pub(crate) struct DoDrop;
 
 impl DropBehavior for NoDrop {
